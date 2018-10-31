dfcdf0a4cec905599870cb841f3d31d615fe3d70
chasemlim <chasemlim@gmail.com>
2018-10-30 20:50:40 -0700

    Update completed dessert_spec in W2D3 HW

--- W2D3_HW/skeleton/spec/dessert_spec.rb
+++ W2D3_HW/skeleton/spec/dessert_spec.rb
@@ -6,37 +6,72 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Gerald") }
+  subject(:icecream) { Dessert.new("Ice Cream", 50, chef)}
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(icecream.type).to eq("Ice Cream")
+    end
+    it "sets a quantity" do
+      expect(icecream.quantity).to eq(50)
+    end
 
-    it "sets a quantity"
+    it "starts ingredients as an empty array" do
+      expect(icecream.ingredients).to eq([])
+    end
 
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    context "when quantity isn't an Integer" do
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Bagel", "five", chef) }.to raise_error(ArgumentError)
+    end
+  end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      icecream.add_ingredient("milk")
+      expect(icecream.ingredients).to include("milk")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["milk", "cream", "chocolate", "ice"]
+
+      ingredients.each do |i|
+        icecream.ingredients << i
+      end
+
+      expect(icecream.mix!).not_to eq(ingredients.shuffle)
+      expect(icecream.ingredients.sort).to eq(ingredients.sort)
+    end
+
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      icecream.eat(10)
 
-    it "raises an error if the amount is greater than the quantity"
+      expect(icecream.quantity).to eq(40)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{icecream.eat(100)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Gerald the Great Baker")
+      expect(icecream.serve).to eq("Gerald the Great Baker has made 50 Ice Creams!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      allow(chef).to receive(:bake).with(icecream)
+      icecream.make_more
+    end
   end
 end
